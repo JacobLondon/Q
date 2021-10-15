56306cf3b1dc217586bb6d6d0754aa3936272b26
Jacob London <jelondon12@gmail.com>
2021-10-14 20:16:00 -0700

    More commands and data folder stuff

--- q.py
+++ q.py
@@ -3,19 +3,45 @@ from discord.ext import commands
 import asyncio
 import discord
 import os
+import sys
 import json
 #import signal
 
+def arg_check(args: list, da, ddarg):
+    return da in args or ddarg in args
+
+def arg_get(args: list, da, ddarg):
+    try:
+        idx = args.index(da)
+    except:
+        try:
+            idx = args.index(ddarg)
+        except:
+            return None
+    if idx + 1 < len(args):
+        return args[idx + 1]
+    return None
+
+# load data/private token info
+pflag = arg_get(sys.argv, "-p", "--path")
+if pflag is None:
+    print('Error: Expected -p PATH to a qdata.json with "token": "XYZ"')
+    exit(1)
+
+data = {}
 everything = {}
 
+NO_SONG = "Nothing"
+DATA_FILE = f"{pflag}/qdata.json"
+
 try:
-    with open("../qdata/qpath.json", "r") as fp:
-        qpath = json.load(fp)
+    with open(DATA_FILE, "r") as fp:
+        qdata = json.load(fp)
 except:
-    qpath = {"shortcuts":{}}
+    qdata = {"token": '', "shortcuts": {}, "modules": []}
 
 def get_songs(path) -> List[str]:
-    target = qpath['shortcuts'].get(path)
+    target = qdata['shortcuts'].get(path)
     if target is None:
         target = path
 
@@ -39,15 +65,26 @@ class Instance:
         self.voice_channel_name = self.voice_channel.name
         self.voice_client = None
 
+        self.whats_playing = NO_SONG
         self._queue = [] # list of all queued music
         self.source = None
         self.do_skip = False
+        self.do_loop = False
+        self.do_front = False # queue in the front instead of back
 
         if self.id in everything.keys():
             self._has_joined = False
         else:
             self._has_joined = True
             everything[self.id] = self
+
+    def reset(self):
+        self.whats_playing = NO_SONG
+        self._queue = []
+        self.source = None
+        self.do_skip = False
+        self.do_loop = False
+        self.do_front = False
 
     def has_joined(self):
         return self._has_joined
@@ -60,20 +97,44 @@ class Instance:
             self._queue.append(path)
         elif isinstance(path, list):
             self._queue.extend(path)
+        
+    def queue_front(self, path: List[str]):
+        self._queue = path + self._queue
 
 def fetch_instance(context):
-    myid = context.guild.id
-    if myid in everything.keys():
-        return everything[myid]
-    return None
+    # the instance or None
+    return everything.get(context.guild.id)
 
-bot = commands.Bot(command_prefix = "Q, ")
+def decorator(command):
+    return {'name': command, 'description': COMMAND_LOOKUP[command], 'pass_context': True}
 
-@bot.command(
-    name='q',
-    description="Queues",
-    pass_context=True,
-)
+COMMAND_PREFIX = "Q, "
+bot = commands.Bot(command_prefix=COMMAND_PREFIX)
+COMMAND_LOOKUP = {
+    "help": f"           View this help. Ex. '{COMMAND_PREFIX}help'",
+    "q": f"              Queues a song. Ex. '{COMMAND_PREFIX}q SONG.mp3'",
+    "list": f"           Lists all queued songs. Ex. '{COMMAND_PREFIX}list'",
+    "leave": f"          Disconnects from the voice chat. Ex. '{COMMAND_PREFIX}leave'",
+    "loop": f"           Loops the current song. See noloop to stop. Will loop next after a 'skip'. Ex. '{COMMAND_PREFIX}loop'",
+    "noloop": f"         Stops looping the current song.. Ex. '{COMMAND_PREFIX}loop'",
+    "play": f"           Starts to play the queue or a song (optional). Ex. '{COMMAND_PREFIX}play [SONG.mp3]'",
+    "stop": f"           Stops playing the current song. Ex. '{COMMAND_PREFIX}stop'",
+    "skip": f"           Skips the current song. Ex. '{COMMAND_PREFIX}skip'",
+    "clear": f"          Clears the queue. Ex. '{COMMAND_PREFIX}clear'",
+    "join": f"           Join the voice chat that you are in. Ex. '{COMMAND_PREFIX}join'",
+    "songname": f"       Display the current song being played. Ex. '{COMMAND_PREFIX}songname'",
+    "commands": f"       Like the help menu but better. Ex. '{COMMAND_PREFIX}commands'",
+}
+
+@bot.command(**decorator('commands'))
+async def qhelp(context):
+    builder = '```\nQ Bot\n==========================================================\n'
+    for key, value in COMMAND_LOOKUP.items():
+        builder += f"{key}\t{value}\n"
+    builder += '```'
+    await context.message.channel.send(builder)
+
+@bot.command(**decorator('q'))
 async def qq(context):
     if (myinstance := fetch_instance(context)) is None:
         return
@@ -81,16 +142,25 @@ async def qq(context):
     # message 'Q, q WHAT I'M INTERESTED IN', get the interested part
     text = context.message.content
     split = text.split()
+    if len(split) < 3:
+        return
     path = split[2:]
     joined = ' '.join(path)
     songs = get_songs(joined)
-    myinstance.queue(songs)
 
-@bot.command(
-    name='list',
-    description="Lists",
-    pass_context=True,
-)
+    if myinstance.do_front:
+        myinstance.queue_front(songs)
+        myinstance.do_front = False
+    else:
+        myinstance.queue(songs)
+
+@bot.command(**decorator('songname'))
+async def qwhatsplaying(context):
+    if (myinstance := fetch_instance(context)) is None:
+        return
+    await context.message.channel.send(myinstance.whats_playing)
+
+@bot.command(**decorator('list'))
 async def qlist(context):
     if (myinstance := fetch_instance(context)) is None:
         return
@@ -100,11 +170,7 @@ async def qlist(context):
     text = '\n'.join(builder)
     await context.message.channel.send(text)
 
-@bot.command(
-    name='leave',
-    description="Leaves",
-    pass_context=True,
-)
+@bot.command(**decorator('leave'))
 async def qleave(context):
     if (myinstance := fetch_instance(context)) is None:
         return
@@ -112,15 +178,32 @@ async def qleave(context):
     await myinstance.voice_client.disconnect()
     del everything[myinstance.id]
 
-@bot.command(
-    name='play',
-    description='Plays',
-    pass_context=True,
-)
+@bot.command(**decorator('loop'))
+async def qloop(context):
+    if (myinstance := fetch_instance(context)) is None:
+        return
+    myinstance.do_loop = True
+
+@bot.command(**decorator('noloop'))
+async def qnoloop(context):
+    if (myinstance := fetch_instance(context)) is None:
+        return
+    myinstance.do_loop = False
+
+@bot.command(**decorator('play'))
 async def qplay(context):
     if (myinstance := fetch_instance(context)) is None:
         return
-    if myinstance.voice_client and myinstance.voice_client.is_playing():
+
+    if myinstance.voice_client:
+        if myinstance.voice_client.is_playing():
+            myinstance.do_front = True
+            await qq(context)
+            await qskip(context)
+            return
+        else:
+            await qq(context)
+    else:
         return
 
     while True:
@@ -135,52 +218,44 @@ async def qplay(context):
             await context.message.channel.send(f"Can't find {path}")
             return
 
-        await context.message.channel.send(f"Hey, coming up next we got {path}")
-        myinstance.voice_client.play(myinstance.source)
-        while myinstance.voice_client.is_playing():
-            await asyncio.sleep(0.25)
-            if myinstance.do_skip:
-                break
-        myinstance.voice_client.stop()
+        while True:
+            #await context.message.channel.send(f"Hey, coming up next we got {path}")
+            myinstance.whats_playing = path
+            myinstance.voice_client.play(myinstance.source)
+            while myinstance.voice_client.is_playing():
+                await asyncio.sleep(0.25)
+                if myinstance.do_skip:
+                    break
 
-@bot.command(
-    name='stop',
-    description='Stops',
-    pass_context=True,
-)
+            myinstance.voice_client.stop()
+            myinstance.whats_playing = ''
+            if not myinstance.do_loop or myinstance.do_skip:
+                break
+        pass
+
+@bot.command(**decorator('stop'))
 async def qstop(context):
     if (myinstance := fetch_instance(context)) is None:
         return
     if myinstance.voice_client and not myinstance.voice_client.is_playing():
         return
     myinstance.voice_client.stop()
+    myinstance.reset()
 
-@bot.command(
-    name='skip',
-    description='Skips',
-    pass_context=True,
-)
+@bot.command(**decorator('skip'))
 async def qskip(context):
     if (myinstance := fetch_instance(context)) is None:
         return
     myinstance.do_skip = True
 
-@bot.command(
-    name='clear',
-    description='Clears',
-    pass_context=True,
-)
+@bot.command(**decorator('clear'))
 async def qclear(context):
     if (myinstance := fetch_instance(context)) is None:
         return
     await qstop(context)
     myinstance._queue = []
 
-@bot.command(
-    name='join',
-    description='Joins',
-    pass_context=True,
-)
+@bot.command(**decorator('join'))
 async def qjoin(context):
     # grab the user who sent the command
     myinstance = Instance(context)
@@ -207,6 +282,7 @@ async def qjoin(context):
 #    asyncio.get_event_loop().run_until_complete(async_cleanup())
 #signal.signal(signal.SIGINT, sig_cleanup)
 
-with open('../qdata/qtoken.txt', 'r') as fd:
-    token = fd.read()
-bot.run(token)
+## TODO: Add loopall
+## TODO: Add noloop (cancels loop and loopall)
+
+bot.run(qdata["token"])
